0e2f9ddeae64fabf5a105ae4a086db20f1cb9238
tkriviradev <tkriviradev@gmail.com>
2017-02-10 10:30:56 +0200

    Fix some files

---
+++ OutlookCalDavSynchronizer2.15.1-source/OutlookCalDav/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName  = 'outlook-caldav-synchronizer'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#Based on NO DETECTION IN PRO
+$packageArgs = @{
+  packageName   = $packageName
+  softwareName  = 'OutlookCalDav*'
+  fileType      = 'zip'
+  silentArgs    = "/qn" # NSIS
+  validExitCodes= @(0) #please insert other valid exit codes here
+  url           = "https://github.com/aluxnimm/outlookcaldavsynchronizer/releases/download/v2.15.1/OutlookCalDavSynchronizer-2.15.1.zip"  #download URL, HTTPS preferrred
+  destination   = $toolsDir
+  file			= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\\CalDavSynchronizer.Setup.msi"
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+$packageArgs.fileType = 'msi'
+Install-ChocolateyInstallPackage @packageArgs
