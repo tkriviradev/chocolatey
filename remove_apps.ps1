580e93a277b820056d5714343b14217bc3cb31a6
tkriviradev <tkriviradev@gmail.com>
2017-03-05 21:09:30 +0200

    Script update
    I just updated the scirpt with more apps. and remove some apps also.

--- remove_apps.ps1
+++ remove_apps.ps1
@@ -6,23 +6,25 @@
 
 #if ($CheckFile) {continue} else {exit}
 
-Get-AppxPackage *3dbuilder* | Remove-AppxPackage
-Get-AppxPackage *officehub* | Remove-AppxPackage
-Get-AppxPackage *skypeapp* | Remove-AppxPackage
-Get-AppxPackage *getstarted* | Remove-AppxPackage
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
-Get-AppxPackage *onenote* | Remove-AppxPackage
-Get-AppxPackage *XboxApp* | Remove-AppxPackage
-Get-AppxPackage *OneConnect* | Remove-AppxPackage
-Get-AppxPackage *XboxGame* | Remove-AppxPackage
-Get-AppxPackage *XboxIdentity* | Remove-AppxPackage
-Get-AppxPackage *Office.sway* | Remove-AppxPackage
+Get-AppxPackage -allusers *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage -allusers *officehub* | Remove-AppxPackage
+Get-AppxPackage -allusers *skypeapp* | Remove-AppxPackage
+Get-AppxPackage -allusers *getstarted* | Remove-AppxPackage
+Get-AppxPackage -allusers *ZuneMusic* | Remove-AppxPackage
+Get-AppxPackage -allusers *ZuneVideo* | Remove-AppxPackage
+Get-AppxPackage -allusers *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage -allusers *XboxApp* | Remove-AppxPackage
+Get-AppxPackage -allusers *OneConnect* | Remove-AppxPackage
+Get-AppxPackage -allusers *XboxIdentity* | Remove-AppxPackage
+Get-AppxPackage -allusers *Office.sway* | Remove-AppxPackage
+Get-AppxPackage -allusers *Office.OneNote* | Remove-AppxPackage
+Get-AppxPackage -allusers *WindowsFeedbackHub* | Remove-AppxPackage
+Get-AppxPackage -allusers *Advertising* | Remove-AppxPackage
+
 
 
 # Prevents "Suggested Applications" returning
-force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content"
+mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content"
 sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content" "DisableWindowsConsumerFeatures" 1
 
 # Prevents "Suggested Applications" returning
